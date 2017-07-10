4aa325cc8ee170c254ff82a7b82dd4fda476dfc1
gemerden <gemerden@gmail.com>
2017-07-10 11:35:26 +0200

    adds google api example

---
+++ samples/google_route.py
@@ -0,0 +1,396 @@
+google_route = {
+    "routes": [
+        {
+            "overview_polyline": {
+                "points": "a{z|HyclYiB_HEEEEEWQw@}@wEAg@Fk@jAqCdBmDnDeGpDsFLODY|@_AbBqB|CkDrCyCrBiCnAkB~AwCx@gBl@wAjA_DtCcJpAiEvBwHbJu]pByHlBaIdB_IpBqIdH_XnDsNjA{El@uCbC{JT]DGpByIh@oCjAuGl@gCTy@`@y@b@i@d@_@n@U\\Eh@?j@Jp@Zz@n@lBbBZJp@p@hBnBjClDdC`EdBjDxBzEH`@dAnBb@n@zC`EvAbBbEtEpDlDnCdC`DtClBjBj@j@RFr@z@dApAbAxApCnEfBhDx@nBhAnDp@zCd@vCP`B^fFZlGVzCd@xCl@|Bv@lBfAhBz@dAjA~@x@b@fA^bALv@Dx@E~AU|@_@`@SdAy@xA_BjDgFfAyAfAoA~A{AdAu@fAq@fBy@`A[~@U|RsD|Bc@dBg@bBk@pAk@lAq@lAw@nB_BxAwAhD}DlAyAHKPALIxAcBjD}Db@[PCV?N@XN`@b@VXJZBFBJ@X?ZaAxBmApCcAvBaBxDITBPF`@r@tDTz@LZR\\RRbFpDdAv@zAx@RL`@XPRr@v@d@h@^d@LBD@JS`AgBp@_Ab@a@|AaAlBoAbBy@v@QpAGfB@vJUdESlAALBh@BVDb@Rp@\\^f@`AnARa@`@]dCyB`DiCfC}AnA}@jAkAvAeAj@W`@KIm@QBa@T{CxBi@d@"
+            },
+            "warnings": [],
+            "bounds": {
+                "northeast": {
+                    "lat": 52.0810438,
+                    "lng": 4.3729629
+                },
+                "southwest": {
+                    "lat": 52.0127067,
+                    "lng": 4.3271703
+                }
+            },
+            "waypoint_order": [],
+            "summary": "Utrechtsebaan/A12",
+            "copyrights": "Map data \u00a92017 Google",
+            "legs": [
+                {
+                    "distance": {
+                        "text": "11.2 km",
+                        "value": 11244
+                    },
+                    "traffic_speed_entry": [],
+                    "end_address": "2611 HH Delft, Netherlands",
+                    "via_waypoint": [],
+                    "start_address": "2595 TZ The Hague, Netherlands",
+                    "start_location": {
+                        "lat": 52.0800134,
+                        "lng": 4.3271703
+                    },
+                    "steps": [
+                        {
+                            "html_instructions": "Head <b>northeast</b> on <b>Prins Willem-Alexanderweg</b> toward <b>Prins Bernhardviaduct</b>",
+                            "distance": {
+                                "text": "0.1 km",
+                                "value": 122
+                            },
+                            "travel_mode": "DRIVING",
+                            "start_location": {
+                                "lat": 52.0800134,
+                                "lng": 4.3271703
+                            },
+                            "polyline": {
+                                "points": "a{z|HyclYWw@e@kBOm@[mACAACEE"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 14
+                            },
+                            "end_location": {
+                                "lat": 52.0805958,
+                                "lng": 4.3286669
+                            }
+                        },
+                        {
+                            "html_instructions": "Slight <b>right</b> onto <b>Prins Bernhardviaduct</b>",
+                            "distance": {
+                                "text": "0.1 km",
+                                "value": 109
+                            },
+                            "travel_mode": "DRIVING",
+                            "maneuver": "turn-slight-right",
+                            "start_location": {
+                                "lat": 52.0805958,
+                                "lng": 4.3286669
+                            },
+                            "polyline": {
+                                "points": "w~z|HemlYEWCOMg@{@oE"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 19
+                            },
+                            "end_location": {
+                                "lat": 52.0810221,
+                                "lng": 4.3301096
+                            }
+                        },
+                        {
+                            "html_instructions": "Turn <b>right</b> to merge onto <b>Utrechtsebaan</b>/<b>A12</b> toward <b>Rotterdam</b>/<b>Utrecht</b>/<b>Amsterdam</b>",
+                            "distance": {
+                                "text": "3.2 km",
+                                "value": 3202
+                            },
+                            "travel_mode": "DRIVING",
+                            "start_location": {
+                                "lat": 52.0810221,
+                                "lng": 4.3301096
+                            },
+                            "polyline": {
+                                "points": "ka{|HevlYAGAI?I?K?G?E?I@EDU^_Aj@qAp@wAr@uAZg@JShAmB|@{A|@yArByCLODY|@_AxAeBHKV[Z]`AeAf@k@VYZ[\\]`AeApA}A`@k@`@m@l@}@h@}@t@yAp@yAFMVi@Tm@dAsCDKp@uBt@}BX{@Rs@Tu@z@sC~@aDv@uCzA{FzA{FpB_Ix@}Cx@_Dv@yCt@yCv@gDXkATkAVmA\\yAZwA^}ATaA^yAj@wBnBqHlBqHZcAPm@bAaEdAmERu@`@_Bh@{B`@oBJe@bC{J"
+                            },
+                            "duration": {
+                                "text": "3 mins",
+                                "value": 159
+                            },
+                            "end_location": {
+                                "lat": 52.0641794,
+                                "lng": 4.367164700000001
+                            }
+                        },
+                        {
+                            "html_instructions": "Take the <b>E19</b> exit toward <b>Rotterdam</b>",
+                            "distance": {
+                                "text": "4.4 km",
+                                "value": 4395
+                            },
+                            "travel_mode": "DRIVING",
+                            "maneuver": "ramp-right",
+                            "start_location": {
+                                "lat": 52.0641794,
+                                "lng": 4.367164700000001
+                            },
+                            "polyline": {
+                                "points": "cxw|Hw}sYT]BE@AT_APw@R{@t@eDd@_CBOXuAp@_ENi@F]H_@J_@H]J[Ra@LWPUPSPQRMRKZINCLALAZ@P@XHZLTL^XZTXV^Z^\\RPZJp@p@n@n@x@~@^d@n@x@TXd@r@t@fAT`@`@p@Vd@\\n@Xh@LX^v@Rb@dBvDF\\@Bv@zALRPVPV`@j@p@~@d@j@`@h@f@l@n@t@hBtBxA~At@t@fAfA`@^PNTTp@l@TPp@n@vAlAhAfAt@r@v@v@h@h@@@RFr@z@`@d@b@j@^h@b@n@V\\j@z@NV|@|ANVj@dA\\p@LXN\\LVLZL\\Vr@L`@Vx@J^J`@FVH^R`AFZF`@F\\Lz@Fh@Hv@JjARzCNnDJ|AFfANrANfATpAT`AVz@JVN^FRR`@\\n@h@x@^f@Z\\l@h@\\TXP^PPHt@Td@H\\B\\BX@f@CPAd@Gd@GPC@AZI`@U`@SbAw@@Ax@y@^e@vAuBrAqB`@m@d@k@r@{@RSf@g@v@s@d@]^W^Wf@Y|@c@h@UTIj@QRGj@MzCk@hB_@nDo@fEw@"
+                            },
+                            "duration": {
+                                "text": "3 mins",
+                                "value": 167
+                            },
+                            "end_location": {
+                                "lat": 52.0345961,
+                                "lng": 4.352954599999999
+                            }
+                        },
+                        {
+                            "html_instructions": "Continue onto <b>A13</b>/<b>E19</b>",
+                            "distance": {
+                                "text": "0.6 km",
+                                "value": 632
+                            },
+                            "travel_mode": "DRIVING",
+                            "start_location": {
+                                "lat": 52.0345961,
+                                "lng": 4.352954599999999
+                            },
+                            "polyline": {
+                                "points": "g_r|H}dqYpAWj@K|@Wf@Oz@Yf@Qf@Uh@Uf@Wd@Yd@[f@[v@k@v@s@b@a@t@u@t@y@p@w@RUl@u@d@i@f@o@HK"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 23
+                            },
+                            "end_location": {
+                                "lat": 52.0294721,
+                                "lng": 4.356675399999999
+                            }
+                        },
+                        {
+                            "html_instructions": "Take exit <b>8-Delft-Noord</b> toward <b>Delft-Noord</b>/<b>Rijswijk-Zuid</b>/<b>Nootdorp</b>",
+                            "distance": {
+                                "text": "0.3 km",
+                                "value": 303
+                            },
+                            "travel_mode": "DRIVING",
+                            "maneuver": "ramp-right",
+                            "start_location": {
+                                "lat": 52.0294721,
+                                "lng": 4.356675399999999
+                            },
+                            "polyline": {
+                                "points": "e_q|Hg|qYPAFCDE`@c@v@_Ax@_Av@}@`@e@VYLKFEFEDCFAHAJAJ@F?F@JDLHLLRTVX"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 22
+                            },
+                            "end_location": {
+                                "lat": 52.0271116,
+                                "lng": 4.357954299999999
+                            }
+                        },
+                        {
+                            "html_instructions": "Turn <b>right</b> onto <b>Vrijenbanselaan</b> (signs for <b>Delft-Noord</b>/<b>Rijswijk-Zuid</b>/<b>Den Haag - Ypenburg</b>/<b>Nootdorp</b>)",
+                            "distance": {
+                                "text": "0.3 km",
+                                "value": 264
+                            },
+                            "travel_mode": "DRIVING",
+                            "maneuver": "turn-right",
+                            "start_location": {
+                                "lat": 52.0271116,
+                                "lng": 4.357954299999999
+                            },
+                            "polyline": {
+                                "points": "mpp|HedrYJZ@B@B@D@D?D@F?D?D?ZaAxB_@x@m@vAEFCFi@hAO\\CFs@~A"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 33
+                            },
+                            "end_location": {
+                                "lat": 52.0283447,
+                                "lng": 4.354979699999999
+                            }
+                        },
+                        {
+                            "html_instructions": "Turn <b>left</b> to stay on <b>Vrijenbanselaan</b>",
+                            "distance": {
+                                "text": "0.5 km",
+                                "value": 542
+                            },
+                            "travel_mode": "DRIVING",
+                            "maneuver": "turn-left",
+                            "start_location": {
+                                "lat": 52.0283447,
+                                "lng": 4.354979699999999
+                            },
+                            "polyline": {
+                                "points": "cxp|HsqqYUf@Sh@ITBPBNBPBPBH@Fh@pCLf@FRDJFNJPFJJHFHJFxAbArAbAh@`@VTl@`@LHlAn@JFFD`@XPRFFj@n@^b@DD"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 65
+                            },
+                            "end_location": {
+                                "lat": 52.0251432,
+                                "lng": 4.3503549
+                            }
+                        },
+                        {
+                            "html_instructions": "Turn <b>left</b> onto <b>Broekmolenweg</b>",
+                            "distance": {
+                                "text": "0.2 km",
+                                "value": 153
+                            },
+                            "travel_mode": "DRIVING",
+                            "maneuver": "turn-left",
+                            "start_location": {
+                                "lat": 52.0251432,
+                                "lng": 4.3503549
+                            },
+                            "polyline": {
+                                "points": "cdp|HutpYNPDHDDBBD@B@B?B@@?DIDI`@u@^q@V_@X_@b@a@"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 66
+                            },
+                            "end_location": {
+                                "lat": 52.0240612,
+                                "lng": 4.351240499999999
+                            }
+                        },
+                        {
+                            "html_instructions": "Continue onto <b>Vrijenbanselaan</b>",
+                            "distance": {
+                                "text": "0.7 km",
+                                "value": 719
+                            },
+                            "travel_mode": "DRIVING",
+                            "start_location": {
+                                "lat": 52.0240612,
+                                "lng": 4.351240499999999
+                            },
+                            "polyline": {
+                                "points": "k}o|HgzpY|AaAPOzA_Aj@Yv@_@LCh@M`AGN?b@?bA@~GQR?^Ab@AN?rBM`AElAALB\\@J@J@"
+                            },
+                            "duration": {
+                                "text": "2 mins",
+                                "value": 104
+                            },
+                            "end_location": {
+                                "lat": 52.0177349,
+                                "lng": 4.352548
+                            }
+                        },
+                        {
+                            "html_instructions": "Continue onto <b>Wateringsevest</b>",
+                            "distance": {
+                                "text": "0.1 km",
+                                "value": 111
+                            },
+                            "travel_mode": "DRIVING",
+                            "start_location": {
+                                "lat": 52.0177349,
+                                "lng": 4.352548
+                            },
+                            "polyline": {
+                                "points": "yun|HmbqYJBLDTLb@TLFNRNRj@p@"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 24
+                            },
+                            "end_location": {
+                                "lat": 52.0168584,
+                                "lng": 4.351827399999999
+                            }
+                        },
+                        {
+                            "html_instructions": "Turn <b>left</b> onto <b>Noordeinde</b>",
+                            "distance": {
+                                "text": "0.2 km",
+                                "value": 228
+                            },
+                            "travel_mode": "DRIVING",
+                            "maneuver": "turn-left",
+                            "start_location": {
+                                "lat": 52.0168584,
+                                "lng": 4.351827399999999
+                            },
+                            "polyline": {
+                                "points": "kpn|H}}pYT\\JSFMHGHIFEDEFGDCr@q@bA{@fA}@dAy@"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 56
+                            },
+                            "end_location": {
+                                "lat": 52.0151014,
+                                "lng": 4.3532147
+                            }
+                        },
+                        {
+                            "html_instructions": "Continue onto <b>Oude Delft</b>",
+                            "distance": {
+                                "text": "0.3 km",
+                                "value": 294
+                            },
+                            "travel_mode": "DRIVING",
+                            "start_location": {
+                                "lat": 52.0151014,
+                                "lng": 4.3532147
+                            },
+                            "polyline": {
+                                "points": "ken|HqfqYHGHIbAm@bAo@h@[d@a@PSx@w@j@_@j@e@j@W`@K"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 79
+                            },
+                            "end_location": {
+                                "lat": 52.0127067,
+                                "lng": 4.3549915
+                            }
+                        },
+                        {
+                            "html_instructions": "Make a <b>U-turn</b> at <b>Schoolstraat</b>",
+                            "distance": {
+                                "text": "0.2 km",
+                                "value": 170
+                            },
+                            "travel_mode": "DRIVING",
+                            "maneuver": "uturn-left",
+                            "start_location": {
+                                "lat": 52.0127067,
+                                "lng": 4.3549915
+                            },
+                            "polyline": {
+                                "points": "mvm|HuqqYGa@AKQBa@TyBzAa@\\i@d@"
+                            },
+                            "duration": {
+                                "text": "1 min",
+                                "value": 75
+                            },
+                            "end_location": {
+                                "lat": 52.0140099,
+                                "lng": 4.354291
+                            }
+                        }
+                    ],
+                    "duration": {
+                        "text": "15 mins",
+                        "value": 906
+                    },
+                    "end_location": {
+                        "lat": 52.0140099,
+                        "lng": 4.354291
+                    }
+                }
+            ]
+        }
+    ],
+    "status": "OK",
+    "geocoded_waypoints": [
+        {
+            "place_id": "ChIJhzCQEhq3xUcRCwkrdU0dwNU",
+            "geocoder_status": "OK",
+            "types": [
+                "postal_code"
+            ]
+        },
+        {
+            "place_id": "ChIJx0qWlMS1xUcRovOVcFreABM",
+            "geocoder_status": "OK",
+            "types": [
+                "postal_code"
+            ]
+        }
+    ]
+}
