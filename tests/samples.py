d52a3cda6695a38b0a986a6083187d8ac71cfb0c
gemerden <gemerden@gmail.com>
2017-07-15 21:53:20 +0200

    moved test data to test directory

--- tests/samples.py
+++ tests/samples.py
@@ -1,3 +1,28 @@
+agenda = {
+    "meeting": "progress on project X",
+    "date": "2017-8-14",
+    "start_time": "10:00",
+    "end_time": "11:00",
+    "invited": ["Joe", "Ann", "Boo"],
+    "items": [
+        {
+            "name": "opening",
+            "duration": "5 minutes",
+            "subjects": ["purpose of the meeting"],
+        },
+        {
+            "name": "progress",
+            "duration": "25 minutes",
+            "subjects": ["milestones", "project delays", "actions"],
+        },
+        {
+            "name": "closing",
+            "duration": "5 minutes",
+            "subjects": ["questions", "roundup"],
+        },
+    ]
+}
+
 google_route = {
     "routes": [
         {
