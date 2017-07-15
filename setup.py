0d3dc0ec7c2c542c8ff9d7d43b2db709de8e373a
gemerden <gemerden@gmail.com>
2017-07-15 22:26:44 +0200

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wildpath',
+      version='0.1',
+      description='data structure access utility',
+      author='Lars van Gemerden',
+      author_email='lars@rational-it.com',
+      packages=['tests', 'wildpath'],
+)
