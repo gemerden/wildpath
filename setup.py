d116cbca558a9445d6db06eebf7bfa5b17719fcd
gemerden <gemerden@gmail.com>
2017-08-10 20:21:10 +0200

    text layout

--- setup.py
+++ setup.py
@@ -32,8 +32,6 @@ setup(
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
