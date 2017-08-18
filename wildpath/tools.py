7ed2c73a7c926b78dc237bf8b38a745f0f60b2e2
gemerden <gemerden@gmail.com>
2017-08-18 17:14:48 +0200

    adds some tools

--- wildpath/tools.py
+++ wildpath/tools.py
@@ -20,3 +20,21 @@ def flatten(item_s):
         out.append(item_s)
     return out
 
+class encoder(object):
+    def __init__(self, *encoders):
+        self.encoders = encoders
+
+    def __call__(self, value):
+        for encoder in self.encoders:
+            value = encoder(value)
+        return value
+
+
+class caller(object):
+    def __init__(self, encoders, **kwargs):
+        self.encoder = encoder(*encoders)
+        self.kwargs = kwargs
+
+    def __call__(self, callable_):
+        return self.encoder(callable_(**self.kwargs))
+
