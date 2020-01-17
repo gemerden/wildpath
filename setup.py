1f3b08b3d66f6547f3ccc457394ec766ee081ea0
gemerden <gemerden@gmail.com>
2020-01-17 14:16:49 +0100

    simplified

--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
-import os
-from codecs import open
 from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Get the long description from the README file; convert to .rst is possible
-try:
-    import pypandoc
-    long_description = pypandoc.convert_file(os.path.join(here, 'README.md'), 'rst')
-except (IOError, ImportError):
-    with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
 
 setup(
     name='wildpath3',
     version='0.3.0',
     description='easy data structure access utility',
-    long_description=long_description,
+    long_description='see <https://github.com/gemerden/wildpath>',
     author='Lars van Gemerden',
     author_email='gemerden@gmail.com',
     license='MIT License',
@@ -36,13 +25,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3',
     ],
-    # python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    python_requires='>=3.3',
     keywords='access data structure getter setter deleter iterator utility tool path wildcard slice',
 )
